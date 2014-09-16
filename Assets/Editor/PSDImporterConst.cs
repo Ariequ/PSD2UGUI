5588f993d1f22195ee4160cbdc3c7049eafc3974
Ariequ <Ariequ>
2014-09-16 19:00:10 +0800

    create new repository

---
+++ Assets/Editor/PSDImporterConst.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PSDImporterConst
+{    
+    public const string BASE_FOLDER = "UI/";
+    public const string PNG_SUFFIX = ".png";
+    public const string COMMON_BASE_FOLDER = "Assets/PSDExportMaterials/";
+
+    public const string RENDER = "Render";
+    public const string NINE_SLICE = "9Slice";
+    public const string IMAGE = "Image";
+
+    public const string PREFAB_PATH_BUTTON = "Prefabs/UI/Button";
+    public const string PREFAB_PATH_CANVAS = "Prefabs/UI/Canvas";
+    public const string PREFAB_PATH_GRID = "Prefabs/UI/Grid";
+    public const string PREFAB_PATH_IMAGE = "Prefabs/UI/Image";
+    public const string PREFAB_PATH_SCROLLVIEW = "Prefabs/UI/ScrollView";
+    public const string PREFAB_PATH_TEXT = "Prefabs/UI/Text";
+}
